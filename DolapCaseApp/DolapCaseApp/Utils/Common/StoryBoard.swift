6ccc6f551492a18c27d7ae3780d583cd55b38e9e
omur olgunay <omur.olgunay@gmail.com>
2019-11-17 10:22:33 +0300

    Add storyboard struct

---
+++ DolapCaseApp/DolapCaseApp/Utils/Common/StoryBoard.swift
@@ -0,0 +1,13 @@
+//
+//  StoryBoard.swift
+//  DolapCaseApp
+//
+//  Created by omur olgunay on 15.11.2019.
+//  Copyright © 2019 omur olgunay. All rights reserved.
+//
+
+import UIKit
+
+struct StoryBoard {
+    static let productDetail = UIStoryboard(name: "ProductDetail", bundle: nil)
+}
